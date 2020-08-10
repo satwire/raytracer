1f3da20472ae9bf5249f2843dcb76296f383b024
Satwiko Wirawan Indrawanto <satwikowirawan@live.com>
2020-08-10 13:37:49 +0200

    Add glm library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/ext/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale
+#include <glm/ext/matrix_clip_space.hpp> // glm::perspective
+
+using namespace glm;
 
 int main()
 {
